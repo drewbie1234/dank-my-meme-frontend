bbd409d17d2e21a61759db29a1883836456aa5b1
drewbie1234 <135838998+drewbie1234@users.noreply.github.com>
2024-05-04 22:13:48 +0100

    adjusting mobile conpatibility, css modules

--- src/components/ContestCreationForm/ContestCreationForm.js
+++ src/components/ContestCreationForm/ContestCreationForm.js
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
-import { ethers } from 'ethers';
 import styles from './ContestCreationForm.module.css';
-import { useWallet } from '../../contexts/WalletContext'; // Update path if necessary
-import { deployContract } from '../../utils/deployContest'; // Make sure this path is correct
+import { useWallet } from '../../contexts/WalletContext';
+import { deployContract } from '../../utils/deployContest';
 
 const ContestCreationForm = ({ onCreate }) => {
     const { selectedAccount, isWalletConnected } = useWallet();
@@ -14,15 +13,12 @@ const ContestCreationForm = ({ onCreate }) => {
         votingFee: '0.1',
         winnerPercentage: '75',
         numberOfLuckyVoters: '1',
-        tokenAddress: '0xe12154f598138d7B77179739DABEDf4AaD80f824' // Assuming DANK is the default token
+        tokenAddress: '0xe12154f598138d7B77179739DABEDf4AaD80f824'
     });
     const [error, setError] = useState('');
     const tokenAddresses = [
         { address: '0xe12154f598138d7B77179739DABEDf4AaD80f824', name: 'DANK' },
     ];
-
-    console.log('selected acc:', selectedAccount);
-    console.log('isWalletConnected:', isWalletConnected);
 
     const handleChange = (e) => {
         const { name, value } = e.target;
@@ -44,10 +40,8 @@ const ContestCreationForm = ({ onCreate }) => {
         try {
             setError('');
             const deployedContractAddress = await deployContract(formData, selectedAccount);
-            console.log('Contract deployed at:', deployedContractAddress);
             onCreate({...formData, contractAddress: deployedContractAddress});
         } catch (deploymentError) {
-            console.error("Deployment Error:", deploymentError);
             setError(`Failed to deploy contract: ${deploymentError.message}`);
         }
     };
@@ -57,43 +51,52 @@ const ContestCreationForm = ({ onCreate }) => {
             <form onSubmit={handleSubmit} className={styles.contestCreationForm}>
                 <h2>Create a New Contest</h2>
                 {error && <p className={styles.error}>{error}</p>}
-                <label>
-                    Contest Name:
+                
+                <div className={styles.formGroup}>
+                    <label>Contest Name:</label>
                     <input type="text" name="name" value={formData.name} onChange={handleChange} />
-                </label>
-                <label>
-                    Start Date:
+                </div>
+
+                <div className={styles.formGroup}>
+                    <label>Start Date:</label>
                     <input type="datetime-local" name="startDateTime" value={formData.startDateTime} onChange={handleChange} />
-                </label>
-                <label>
-                    End Date:
+                </div>
+
+                <div className={styles.formGroup}>
+                    <label>End Date:</label>
                     <input type="datetime-local" name="endDateTime" value={formData.endDateTime} onChange={handleChange} />
-                </label>
-                <label>
-                    Entry Fee:
+                </div>
+
+                <div className={styles.formGroup}>
+                    <label>Entry Fee:</label>
                     <input type="number" step="0.01" name="entryFee" value={formData.entryFee} onChange={handleChange} />
-                </label>
-                <label>
-                    Voting Fee:
+                </div>
+
+                <div className={styles.formGroup}>
+                    <label>Voting Fee:</label>
                     <input type="number" step="0.01" name="votingFee" value={formData.votingFee} onChange={handleChange} />
-                </label>
-                <label>
-                    Winner Percentage:
+                </div>
+
+                <div className={styles.formGroup}>
+                    <label>Winner Percentage:</label>
                     <input type="number" step="1" min="0" max="100" name="winnerPercentage" value={formData.winnerPercentage} onChange={handleChange} />
-                </label>
-                <label>
-                    Number of Lucky Voters:
+                </div>
+
+                <div className={styles.formGroup}>
+                    <label>Number of Lucky Voters:</label>
                     <input type="number" min="1" name="numberOfLuckyVoters" value={formData.numberOfLuckyVoters} onChange={handleChange} />
-                </label>
-                <label>
-                    Token Address:
+                </div>
+
+                <div className={styles.formGroup}>
+                    <label>Token Address:</label>
                     <select name="tokenAddress" value={formData.tokenAddress} onChange={handleChange}>
                         <option value="">Select a Token</option>
                         {tokenAddresses.map(token => (
                             <option key={token.address} value={token.address}>{token.name}</option>
                         ))}
                     </select>
-                </label>
+                </div>
+
                 <button type="submit">Create Contest</button>
             </form>
         </div>
