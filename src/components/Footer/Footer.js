74d5e19067adca0f5bccca97797af8d11c6f50c3
drewbie1234 <135838998+drewbie1234@users.noreply.github.com>
2024-05-04 17:49:26 +0100

    backend setup on dainty cloud

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,15 @@
 import React from 'react';
-import styles from './Footer.module.css'; // Assuming you have a CSS module for styling
-import xLogoSVG from "../../svgs/xLogoSVG.svg"
+import styles from './Footer.module.css';
+import xLogoSVG from "../../svgs/xLogoSVG.svg";
+
+// SVG imports for the social media icons
+import twitterLogo from "../../svgs/twitterLogo.svg";
+import coinMarketCapLogo from "../../svgs/coinMarketCapLogo.svg";
+import coinGeckoLogo from "../../svgs/coinGeckoLogo.svg";
+import dexToolsLogo from "../../svgs/dexToolsLogo.svg";
+import etherscanLogo from "../../svgs/etherscanLogo.svg";
+import duneAnalyticsLogo from "../../svgs/duneAnalyticsLogo.svg";
+
 const Footer = () => {
   return (
     <footer className={styles.footer}>
@@ -8,8 +17,36 @@ const Footer = () => {
         <div className={styles.socialLinks}>
           <h4>Follow Us</h4>
           <ul>
-          <a target="_blank" href="https://twitter.com/CryptoCrus58025"><img src={xLogoSVG} alt="Wallet" style={{ width: '15px', marginLeft: '8px', verticalAlign: 'middle' }} /></a>
-            
+            <li>
+              <a href="https://twitter.com/pond0x" target="_blank" rel="noopener noreferrer">
+                <img src={twitterLogo} alt="Twitter" className={styles.icon} />
+              </a>
+            </li>
+            <li>
+              <a href="https://coinmarketcap.com/currencies/pond0x/" target="_blank" rel="noopener noreferrer">
+                <img src={coinMarketCapLogo} alt="CoinMarketCap" className={styles.icon} />
+              </a>
+            </li>
+            <li>
+              <a href="https://www.coingecko.com/en/coins/pondcoin" target="_blank" rel="noopener noreferrer">
+                <img src={coinGeckoLogo} alt="CoinGecko" className={styles.icon} />
+              </a>
+            </li>
+            <li>
+              <a href="https://www.dextools.io/app/en/ether/pair-explorer/0xc64350c0eab6faed8f17cc2cdff0761c53fb4152" target="_blank" rel="noopener noreferrer">
+                <img src={dexToolsLogo} alt="DEXTools" className={styles.icon} />
+              </a>
+            </li>
+            <li>
+              <a href="https://www.etherscan.io/token/0x423f4e6138E475D85CF7Ea071AC92097Ed631eea" target="_blank" rel="noopener noreferrer">
+                <img src={etherscanLogo} alt="Etherscan" className={styles.icon} />
+              </a>
+            </li>
+            <li>
+              <a href="https://dune.com/mogie/pond-dex" target="_blank" rel="noopener noreferrer">
+                <img src={duneAnalyticsLogo} alt="Dune Analytics" className={styles.icon} />
+              </a>
+            </li>
           </ul>
         </div>
       </div>
