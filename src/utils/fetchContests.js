8b919c0888bb9befa19926014f110f29b704bd71
drewbie1234 <135838998+drewbie1234@users.noreply.github.com>
2024-05-04 15:09:02 +0100

    backend setup on dainty cloud

--- src/utils/fetchContests.js
+++ src/utils/fetchContests.js
@@ -1,5 +1,5 @@
 const fetchContests = async () => {
-    const response = await fetch('https://194.124.43.95:3001/api/contests', {
+    const response = await fetch('http://194.124.43.95:3001/api/contests', {
         method: 'GET', // Use GET method to fetch data
         headers: {
             'Content-Type': 'application/json'
