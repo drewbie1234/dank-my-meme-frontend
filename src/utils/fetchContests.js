afa2ad615adc657b453e499488d508e06c26c1ac
drewbie1234 <135838998+drewbie1234@users.noreply.github.com>
2024-05-05 23:12:19 +0100

    adjusting mobile conpatibility, css modules

--- src/utils/fetchContests.js
+++ src/utils/fetchContests.js
@@ -2,7 +2,7 @@ import { toast } from 'react-toastify';
 
 const fetchContests = async () => {
     try {
-        const response = await fetch('http://194.124.43.95:3001/api/contests', {
+        const response = await fetch('https://194.124.43.95:3001/api/contests', {
             method: 'GET', // Use GET method to fetch data
             headers: {
                 'Content-Type': 'application/json'
