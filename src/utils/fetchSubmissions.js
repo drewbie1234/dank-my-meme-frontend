809805d5f061b490141b1398d36251ea9d0d8147
drewbie1234 <135838998+drewbie1234@users.noreply.github.com>
2024-05-09 19:45:10 +0100

    UI CHANGES TO CONTEST CARD

--- src/utils/fetchSubmissions.js
+++ src/utils/fetchSubmissions.js
@@ -6,7 +6,7 @@ const fetchSubmissions = async (submissionIds) => {
     }
 
     try {
-        const response = await fetch(`https://194.124.43.95:443/api/submissions?submissionIds=${submissionIds.join(',')}`, {
+        const response = await fetch(`http://194.124.43.95:3001/api/submissions?submissionIds=${submissionIds.join(',')}`, {
             method: 'GET', // Use GET method to fetch data
             headers: {
                 'Content-Type': 'application/json'
