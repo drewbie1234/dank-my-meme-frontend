09076c7442db72a6a357f6db4ffd8a42eb9e0a82
ANT\dkillin <drew_killingley@hotmail.com>
2024-06-11 12:28:36 +0100

    dankify tool added

--- src/components/CreatePage/CreatePage.js
+++ src/components/CreatePage/CreatePage.js
@@ -5,7 +5,7 @@ import PromptGenerator from '../PromptGenerator/PromptGenerator';
 import Dankify from '../Dankify/Dankify'; // Assuming this component exists
 
 const CreatePage = () => {
-  const [activeComponent, setActiveComponent] = useState('PepeToPork');
+  const [activeComponent, setActiveComponent] = useState('Dankify');
 
   const renderActiveComponent = () => {
     switch (activeComponent) {
